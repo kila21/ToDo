71ae12ab3f7efc14bce89901bab890fe5e6f4bb1
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 01:44:02 +0400

    layout and table on main page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { Table } from './components/table/table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, DatePipe, Table],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'todo';
+  today: Date = new Date();
 }
