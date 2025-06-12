e78ef8cf27f38a9cd3fca05a90532677818bdacd
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 01:43:00 +0400

    init table system

---
+++ src/app/components/table/table.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  imports: [CommonModule],
+  templateUrl: './table.html',
+  styleUrl: './table.scss'
+})
+export class Table {
+}
