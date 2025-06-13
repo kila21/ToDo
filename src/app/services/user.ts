eb2ce376b6e4672588b44991a887343c21231d48
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 05:22:44 +0400

    get post services

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -2,16 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserResponse } from '../types/UserResponse';
 import { Observable } from 'rxjs';
+import { PostResponse } from '../types/PostResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class User {
-  private apiUrl = 'https://jsonplaceholder.typicode.com/users'
+  private usersApiUrl = 'https://jsonplaceholder.typicode.com/users'
+  private postsApiUrl = 'https://jsonplaceholder.typicode.com/posts'
 
   constructor(private http: HttpClient) { };
 
   getUsers(): Observable<UserResponse[]> {
-    return this.http.get<UserResponse[]>(this.apiUrl)
+    return this.http.get<UserResponse[]>(this.usersApiUrl)
+  }
+  
+  getPosts(): Observable<PostResponse[]> {
+    return this.http.get<PostResponse[]>(this.postsApiUrl)
+  }
+
+  getUserPosts(userId: number): Observable<PostResponse[]> {
+    return this.http.get<PostResponse[]>(`${this.postsApiUrl}?userId=${userId}`)
   }
 }
