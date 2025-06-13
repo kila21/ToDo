b04f6ae39f60b7f51fe3d90d55d74c15f71f6c65
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 06:20:51 +0400

    create route for user posts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Users } from './components/users/users';
 import { Table } from './components/table/table';
 import { Posts } from './components/posts/posts';
+import { UserPosts } from './components/user-posts/user-posts';
 
 export const routes: Routes = [
     {path: '', component: Table},
     {path: 'users', component: Users},
     {path: 'posts', component: Posts},
+    {path: 'posts/:id', component: UserPosts}
 ];
