2e00dd8737aeaed216673af7336ba5dc88b820b8
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 23:37:54 +0400

    create footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule, Router } from '@angular/router';
 import { CommonModule, DatePipe } from '@angular/common';
 import { Table } from './components/table/table';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, DatePipe, CommonModule],
+  imports: [RouterOutlet, RouterModule, DatePipe, CommonModule, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
