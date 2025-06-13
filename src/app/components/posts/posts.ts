1a2e405406d7d1196e11ba042b468c1d6952b824
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 23:28:24 +0400

    update posts page content with table

--- src/app/components/posts/posts.ts
+++ src/app/components/posts/posts.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { PostCard } from '../post-card/post-card';
-import { HttpClient } from '@angular/common/http';
 import { User } from '../../services/user';
 import { PostResponse } from '../../types/PostResponse';
 import { CommonModule } from '@angular/common';
 import { Button } from '../button/button';
+import { UserResponse } from '../../types/UserResponse';
+import { PostPopup } from '../post-popup/post-popup';
 
 @Component({
   selector: 'app-posts',
-  imports: [PostCard, CommonModule, Button],
+  imports: [CommonModule, Button, PostPopup],
   templateUrl: './posts.html',
   styleUrl: './posts.scss'
 })
 
 export class Posts implements OnInit {
   posts: PostResponse[] = []
+  users: {name: string, id: number}[] = []
   postsLength: number = 0
   hidden: boolean = false
+  popup: Boolean = false
+  selectedPost: {title: string, body: string} | undefined;
 
   constructor(private service: User) {}
+
+  togglePopup() {
+    this.popup = !this.popup
+  }
+
+  getSelectedPost(title: string, body: string) {
+    this.selectedPost = {title: title, body: body}
+    this.togglePopup()
+  }
 
   async fetchPosts() {
     this.postsLength += 10
@@ -30,8 +42,17 @@ export class Posts implements OnInit {
     })
   }
 
+  async fetchUsers() {
+    await this.service.getUsers().subscribe((data: UserResponse[]) => {
+      data.forEach((user: UserResponse) => {
+        this.users.push({name: user.name, id: user.id})
+      })
+    })
+  }
+
   ngOnInit(): void {
     this.fetchPosts()
+    this.fetchUsers()
   }
 
 }
