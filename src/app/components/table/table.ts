d5657fc49d1cc02f3c33a3952ddd93d6782456c3
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 06:21:11 +0400

    add links to table

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -2,21 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { UserResponse } from '../../types/UserResponse';
 import { User } from '../../services/user';
+import { Button } from '../button/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-table',
-  imports: [CommonModule],
+  imports: [CommonModule, Button, RouterModule],
   templateUrl: './table.html',
   styleUrl: './table.scss'
 })
 export class Table implements OnInit {
   users: UserResponse[] = []
+  hidden: boolean = false
+  dataLength: number = 0
 
   constructor(private userService: User) {}
 
   async fetchUsers() {
+    this.dataLength += 5
     await this.userService.getUsers().subscribe(data => {
-      this.users = data
+      if (data.length <= this.dataLength ) {
+          this.hidden = true
+      }
+      this.users = data.slice(0, this.dataLength)
     })
   }
 
