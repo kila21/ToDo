ef74ca0f93a3cdf35c0a9d5103f65889c2acffcc
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-13 22:31:45 +0400

    users page

---
+++ src/app/components/users/users.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Table } from '../table/table';
+
+@Component({
+  selector: 'app-users',
+  imports: [Table],
+  templateUrl: './users.html',
+  styleUrl: './users.scss'
+})
+export class Users {
+
+}
