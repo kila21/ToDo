5e6eea73c188eb487db2333da6925b3ff7ed1757
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-14 07:40:29 +0400

    get todo list

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UserResponse } from '../types/UserResponse';
 import { Observable } from 'rxjs';
+
+import { UserResponse } from '../types/UserResponse';
 import { PostResponse } from '../types/PostResponse';
+import { TodoResponse } from '../types/TodoResponse';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,7 @@ import { PostResponse } from '../types/PostResponse';
 export class User {
   private usersApiUrl = 'https://jsonplaceholder.typicode.com/users'
   private postsApiUrl = 'https://jsonplaceholder.typicode.com/posts'
+  private todoApiUrl = 'https://jsonplaceholder.typicode.com/todos'
 
   constructor(private http: HttpClient) { };
 
@@ -23,5 +27,9 @@ export class User {
 
   getUserPosts(userId: number): Observable<PostResponse[]> {
     return this.http.get<PostResponse[]>(`${this.postsApiUrl}?userId=${userId}`)
+  }
+
+  getUserTodo(userId: number): Observable<TodoResponse[]> {
+    return this.http.get<TodoResponse[]>(`${this.todoApiUrl}?userId=${userId}`)
   }
 }
