fd791c59da6386ce2714ff50e399b9b0bf203a40
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-14 07:46:16 +0400

    remove users component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { Users } from './components/users/users';
 import { Posts } from './components/posts/posts';
 import { UserPosts } from './components/user-posts/user-posts';
 import { Todo } from './components/todo/todo';
+import { Table } from './components/table/table';
 
 export const routes: Routes = [
-    {path: 'users', component: Users},
+    {path: 'users', component: Table},
     {path: 'posts', component: Posts},
     {path: 'posts/:id', component: UserPosts},
     {path: 'todo/:id', component: Todo }
--- src/app/components/search/search.spec.ts
+++ src/app/components/search/search.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Users } from './users';
+import { Search } from './search';
 
-describe('Users', () => {
-  let component: Users;
-  let fixture: ComponentFixture<Users>;
+describe('Search', () => {
+  let component: Search;
+  let fixture: ComponentFixture<Search>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Users]
+      imports: [Search]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Users);
+    fixture = TestBed.createComponent(Search);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
